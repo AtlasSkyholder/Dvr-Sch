963b82ef52a8f0fd69bfc83caa11276b4b77e002
AtlasSkyholder <thecanuckatheist@gmail.com>
2020-09-22 18:02:40 -0400

    csv write functionality part 1

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ const PORT       = process.env.PORT || 8080;
 const express    = require("express");
 const multer = require('multer');
 const path = require('path');
+const Papa = require('papaparse');
+const fs = require('fs');
+const bodyParser = require('body-parser');
 
 const app        = express();
 
 const data = require('./assets/data/data');
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
-const Papa = require('papaparse');
-const fs = require('fs');
 let csvFile; // name for file to be parsed after POST
 let csvData; // name for data of csv
 let weekNumber = 1; // variable for cycling through the weeks
@@ -23,18 +25,37 @@ const storage = multer.diskStorage({
   filename: (req, file, cb) => {
       console.log(file);
       csvFile = file.originalname;
-      cb(null, Date.now() + path.extname(file.originalname));
+      cb(null, file.originalname);
   }
 });
 
 const upload = multer({ storage: storage });
+
+
+
+const someArray = [
+                { name: "Fred",
+                  array: [1,2,3]},
+                  {name: "Dude",
+                  array: [[4],[5],[6]]}
+              ];
+const someData = Papa.unparse(someArray);
+
+fs.writeFile("./uploads/someData.csv", someData, err => {
+  if (err) throw err;
+  console.log("someData table successfully saved!");
+});
+
+
+
+
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
 app.post("/", upload.single('myfile'), (req, res) => {
-  const testFilePath = csvFile;
+  const testFilePath = req.file.path;
   const testFile = fs.readFileSync(testFilePath, "utf8");
 
   const testRows = {};
@@ -60,7 +81,20 @@ app.get("/schedule", (req, res) => {
   let strData = JSON.stringify(data);
 
   res.render("schedule", { week, csvData, weekNumber, strData, data });
-})
+});
+
+app.post("/schedule", (req,res) => {
+  
+/*   const animeArray = animeRows.data.map(row => {
+    const { name, anime_id, type, episodes, rating, members } = row;
+    const editedName = name.replace(/,/g, ' ');
+
+    return { name: editedName, anime_id, type, episodes, rating, members }
+  }) */
+
+  console.log(req.body);
+
+});
 
 app.listen(PORT, function(){
   console.log(`Example app listening on port ${PORT}`);
